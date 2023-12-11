bd84dab91268cfca6feaeb46cdc69e1e42b713ff
yunusemretokyay1 <101728985+yunusemretokyay1@users.noreply.github.com>
2023-12-12 00:45:54 +0300

    Update markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -1,50 +1,29 @@
 #!/usr/bin/python3
-"""
-A script that converts Markdown to HTML.
-"""
-
+''' Markdown to HTML '''
 import sys
-import os
-import re
+from os import path
 
-def convert_markdown_to_html(input_file, output_file):
-    """
-    Converts a Markdown file to HTML and writes the output to a file.
-    """
-    # Check that the Markdown file exists and is a file
-    if not (os.path.exists(input_file) and os.path.isfile(input_file)):
-        print(f"Missing {input_file}", file=sys.stderr)
-        sys.exit(1)
-
-    # Read the Markdown file and convertt it to HTML
-    with open(input_file, encoding="utf-8") as f:
-        html_lines = []
-        for line in f:
-            # Check for Markdown headings
-            match = re.match(r"^(#+) (.*)$", line)
-            if match:
-                heading_level = len(match.group(1))
-                heading_text = match.group(2)
-                html_lines.append(f"<h{heading_level}>{heading_text}</h{heading_level}>")
-            else:
-                html_lines.append(line.rstrip())
-
-    # Write the HTML output to a file
-    with open(output_file, "w", encoding="utf-8") as f:
-        f.write("\n".join(html_lines))
 
 if __name__ == "__main__":
-    # Check that the correct number of arguments were provided
+    '''the number of arguments is less than 2'''
     if len(sys.argv) != 3:
-        print("Usage: ./markdown2html.py <input_file> <output_file>", file=sys.stderr)
-        sys.exit(1)
-
-    # Get the input and output file names from the command-line arguments
-    input_file = sys.argv[1]
-    output_file = sys.argv[2]
-
-    # Convert the Markdown file to the HTML and write the output to a file
-    convert_markdown_to_html(input_file, output_file)
-
-    # Exit  with a successful status code
-    sys.exit(0)
+        print('Usage: ./markdown2html.py README.md README.html' ,file=sys.stderr)
+        exit(1)
+    '''Markdown file doesn’t exist'''
+    if not path.exists(sys.argv[1]):
+        print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
+        exit(1)
+    '''Headings Markdown'''
+    with open(sys.argv[1], 'r') as read_file:
+        line_list = []
+        for lines in read_file.readlines():
+            cout_cra = 0
+            for line in lines:
+                for car in range(len(line)):
+                    if line[car] == '#':
+                        cout_cra += 1
+            lines = lines.rstrip('\r\n')
+            line_list.append("<h{}>{}</h{}>".format(cout_cra, lines.replace('#',''), cout_cra))
+        with open(sys.argv[2], 'a') as write_file:
+            for line in line_list:
+                write_file.write('{}\n'.format(line))
